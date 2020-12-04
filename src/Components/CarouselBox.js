c9ba331b094e3c1fe1b7d84d98039dcbcb16d6cb
Floqrrr <dead.samura1@yandex.ru>
2020-12-04 13:24:47 +0700

    fix

--- src/Components/CarouselBox.js
+++ src/Components/CarouselBox.js
@@ -2,31 +2,38 @@ import React, { Component } from 'react'
 import Carousel from 'react-bootstrap/Carousel'
 import lift1 from '../assets/lift1.png';
 import lift2 from '../assets/lift2.jpg';
+import '../Page.css';
+
+
 
 export default class CarouselBox extends Component {
     render() {
         return (
         <Carousel>
-            <Carousel.Item>
+
+            <Carousel.Item style={{'height': '600px'}}>
                 <img 
                     className="d-block w-100"
                     src={lift1}
                     alt="Lift1"
                 />
-                <Carousel.Caption>
+                <Carousel.Caption >
+                    <strong className="ie">
                     <h3>Белые росы</h3>
-                   
+                    </strong>
                 </Carousel.Caption>
             </Carousel.Item>
-            <Carousel.Item>
+            <Carousel.Item style={{'height': '600px'}}>
                 <img 
                     className="d-block w-100"
                     src={lift2}
                     alt="Lift2"
                 />
                 <Carousel.Caption>
+                    <strong>
                     <h3>Тихие зори</h3>
                     <p>...</p>
+                    </strong>
                 </Carousel.Caption>
             </Carousel.Item>
         </Carousel>
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react'
 import { Carousel, Container } from 'react-bootstrap'
 import CarouselBox from '../Components/CarouselBox'
+import '../Page.css';
 
 export default class Home extends Component {
     render() {
         return (
             <dev>
-                <CarouselBox></CarouselBox>
                 
+                <CarouselBox></CarouselBox>
+
+                
+             
                 <Container>
                 <blockquote class="blockquote text-center">
                     
